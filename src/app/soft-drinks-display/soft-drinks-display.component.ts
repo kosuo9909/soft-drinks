2026101ea5226df5ec85763b10f3174aad172bcb
kosuo9909 <stanevbsn@gmail.com>
2024-04-09 11:15:53 +0300

    minor clean up + fix image hover covers the ribbon

--- src/app/soft-drinks-display/soft-drinks-display.component.ts
+++ src/app/soft-drinks-display/soft-drinks-display.component.ts
@@ -5,10 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { IndexedDBService } from '../services/indexed-db.service';
 import { getLocale, setLocale } from '../../locale/i18n';
-
-interface ProductImageMappings {
-  [key: string]: string;
-}
+import { productMappings } from '../../assets/productMappingsMock';
 
 @Component({
   selector: 'app-soft-drinks-display',
@@ -22,28 +19,6 @@ export class SoftDrinksDisplayComponent implements OnInit, OnDestroy {
   private _ngUnsubscribe = new Subject<void>();
   public selectedSortOption: 'price' | 'discount' = 'price';
   public currentLanguage: 'en-GB' | 'bg-BG' | string = 'en-GB';
-  private productMappings: ProductImageMappings = {
-    'Газирана вода Billa Произход – България':
-      'https://mysupermarket.bg/api/public/images/products/2axo2a_1637589490741.png',
-    'Газирана напитка Лимонада Aspasia':
-      'https://mysupermarket.bg/api/public/images/tmarket/f83ff83fe7c30000be40fc01bc01dfe9ffffbff180018001fe41be418400ff1f.webp',
-    'Fanta Газирана напитка 1,5 l/опаковка':
-      'https://mysupermarket.bg/api/public/images/lidl/f81ff83fc1c7a001e007e0f9ffdda001e007f00ff01ff80ff807f007e007ee37.webp',
-    'Газирана напитка Coca Cola или Fanta':
-      'https://myplov.com/image/cache/catalog/Fanta-550x688w.png',
-    'Газирана напитка Pepsi':
-      'https://mysupermarket.bg/api/public/images/tmarket/fc1ff80ff007e003e007f007f807fc4ffc8ff00ff007e007e007e007f00ffc9f.webp',
-    'Газирана напитка PEPSI; MIRINDA; различни видове':
-      'https://mysupermarket.bg/api/public/images/tmarket/fc1ff80ff007e003e007f007f807fc4ffc8ff00ff007e007e007e007f00ffc9f.webp',
-    'Газирана напитка CRODO различни видове':
-      'https://www.supermag.bg/images/2022_3/gazirana-napitka-crodo-oran-soda-330ml.jpg?w=426&h=352',
-    'Газирана напитка Coca Cola':
-      'https://storage.googleapis.com/images-mus-prd-7e2fdbe.mus.prd.v8.commerce.mi9cloud.com/product-images/detail/1414906000_1',
-    'Газирана напитка Pepsi, Mirinda или 7UP':
-      'https://kaufland.media.schwarz/is/image/schwarz/02300260B_P?JGstbGVnYWN5LW9uc2l0ZS0yJA==',
-    'Газирана безалкохолна напитка COCA-COLA; FANTA различни видове':
-      'https://myplov.com/image/cache/catalog/Fanta-550x688w.png',
-  };
 
   public languageNames: { [key: string]: string } = {
     'en-GB': 'Български',
@@ -89,7 +64,7 @@ export class SoftDrinksDisplayComponent implements OnInit, OnDestroy {
     if (cachedUrl) {
       return cachedUrl;
     } else {
-      const url = this.productMappings[productName];
+      const url = productMappings[productName];
       if (url) {
         await this.indexedDBService.setImageUrl(productName, url);
         return url;
